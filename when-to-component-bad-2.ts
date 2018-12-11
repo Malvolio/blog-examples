cc9d1ab2ab03fc3ee0312debd02d69f9452fdcec
Malvolio <mlorton@yahoo.com>
2018-12-11 19:39:45 +0100

    Create when-to-component-bad-2.ts

---
+++ when-to-component-bad-2.ts
@@ -0,0 +1,21 @@
+/**
+ * very bad use of .subscribe()
+ * Do NOT do this either
+ */
+@Component({
+  template: `The current value is {{currentValue}}`,
+})
+export class SomeComponent implements OnDestroy {
+  private readonly onDestroy = new Subject<void>();
+  currentValue: string;
+  constructor(someService: SomeService) {
+    someService.getSomeObservable()
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(v => {
+      this.currentValue = v;
+    });
+  }
+  ngOnDestroy() {
+    this.onDestroy.next();
+  }
+}
