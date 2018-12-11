5fbbbae888de89c21b3f25a1da36fad2c3e76ffc
Malvolio <mlorton@yahoo.com>
2018-12-11 19:47:31 +0100

    Create when-to-component-good-1.ts

---
+++ when-to-component-good-1.ts
@@ -0,0 +1,9 @@
+@Component({
+  template: `The current value is {{currentValue | async }}`,
+})
+export class SomeComponent  {
+  constructor(private someService: SomeService) {}
+  
+  currentValue = this.someService.getSomeObservable()
+                     .pipe(share());
+}
